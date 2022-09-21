6188e9ec65de60728bfafdb612b12f05263cfbaa
code49 <david.le.chan@gmail.com>
2022-09-21 19:54:12 -0400

    initial function setup

--- headerfooter.py
+++ headerfooter.py
@@ -4,3 +4,42 @@ function definitions for creating standardised header and footers to streamlit/h
 
 """
 
+def renderHeader():
+
+    """
+
+    handles creating the streamlit/hydralit components for the header section of each page.
+    
+    Parameters:
+    -----------
+
+    None
+
+    Returns:
+    --------
+
+    None
+
+    """
+
+    pass
+
+def renderFooter():
+
+    """
+
+    handles creating the streamlit/hydralit components for the footer section of each page.
+    
+    Parameters:
+    -----------
+
+    None
+
+    Returns:
+    --------
+
+    None
+
+    """
+
+    pass
