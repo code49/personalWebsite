2df2e568e19db12585ef525fadbf62bb9df1e303
code49 <david.le.chan@gmail.com>
2022-09-21 20:11:07 -0400

    first draft of footer content

--- headerfooter.py
+++ headerfooter.py
@@ -4,6 +4,16 @@ function definitions for creating standardised header and footers to streamlit/h
 
 """
 
+# ----- import libraries -----
+
+# streamlit/hydralit
+import streamlit as st
+# import hydralit as hy # there's a bug with hydralit so disabling for now
+import hydralit_components as hc
+
+# ----- code -----
+
+# page header render function
 def renderHeader():
 
     """
@@ -24,6 +34,7 @@ def renderHeader():
 
     pass
 
+# page footer render function
 def renderFooter():
 
     """
@@ -42,4 +53,54 @@ def renderFooter():
 
     """
 
-    pass
+    # horizontal rule
+    st.markdown("---")
+
+    # markdowns with centering html
+    st.markdown(
+        """
+        <p style='text-align: center; color: grey;'> <span style="font-size: 12px"> 
+        <em> "Guess what? You're going to wake up tomorrow and have an absolutely phenomenal day. 
+        And if your day isn't phenomenal, it'll still be better than today." </em> - Celine "starsmitten" Cheung 
+        </span></p>
+        """, 
+        unsafe_allow_html=True
+    )
+    st.write("") # for spacing
+    st.markdown(
+        """
+        <p style='text-align: center; color: grey;'> <span style="font-size: 15px">
+        David Le Chan
+        </span></p>
+        """, 
+        unsafe_allow_html=True
+    )
+    st.markdown(
+        """
+        <p style='text-align: center; color: grey;'> <span style="font-size: 15px">
+        davidlechan@gmail.com
+        </span></p>
+        """, 
+        unsafe_allow_html=True
+    )
+    st.markdown(
+        """
+        <p style='text-align: center; color: grey;'> <span style="font-size: 15px">
+        +1 (650) - 383 - 8954
+        </span></p>
+        """, 
+        unsafe_allow_html=True
+    )
+    st.markdown(
+        """
+        <p style='text-align: center; color: grey;'> <span style="font-size: 15px">
+        <a href="https://linkedin.com/in/david-le-chan"> linkedin.com/in/david-le-chan </a>
+        </span></p>
+        """, 
+        unsafe_allow_html=True
+    )
+    
+
+    
+
+renderFooter()
