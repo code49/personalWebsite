9e14aff96ae64060fe7bbff918643987017a188a
David Chan <davidlechan@gmail.com>
2022-11-04 14:54:56 -0400

    made spelling consistent

--- about/about.py
+++ about/about.py
@@ -36,7 +36,7 @@ def renderAbout():
 
     columns = st.columns([1, 7, 1]) # 2, 2 margins on the edges
     with columns[1]:
-        st.header("About Me :)")
+        st.header("about me :)")
 
     # --- section 1: intro ---
 
