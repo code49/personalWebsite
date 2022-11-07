04dbeb669fd97dff0eac36d5f11e3d8120d72c1e
code49 <david.le.chan@gmail.com>
2022-11-06 19:32:25 -0500

    updated work on vestaboard experience page

--- experience/vestaboard.py
+++ experience/vestaboard.py
@@ -44,22 +44,42 @@ class RenderVestaboardPage(HydraHeadApp):
         st.markdown(
             """
             ## Vestaboard:
-            _paid programming intern_
+            _paid programming intern_ \n
             _June - August 2021_
+            
+            ---
             """
         )
 
         # --- section 1: intro ---
 
-        st.image("./resources/experience/vestaboard_header")
+        st.image("./resources/experience/vestaboard_header.jpg")
         
         st.write(
             """
             Vestaboard is a startup based in South San Francisco building smart split-flap display boards,
-            combining the look and sound of a old-timey train station while integrating all the smarts of
-            modern day technology.
+            combining the looks and sounds of a cruicial element of old-timey train station while integrating 
+            all the smarts of modern day technology. Users can use their Vestaboard to write a message to their 
+            kids when away on business, see live sports scores, even be inspired by quotes from celebrities 
+            and influencers.
+            """)
+
+        # --- section 2: what I did ---
+
+        st.write(
+            """
+            I worked at Vestaboard to implement Word-Of-The-Day, an "installable" (plugins that control
+            the board, automatically collecting data from the internet to generate messages) that polled the 
+            English Oxford Dictionary's word of the day to display to the board. Creating my installable
+            required that I write many new features, including RSS feeds, pagination (getting the server
+            to queue and send a group of messages in order, since definitions took more space than a single 'frame'
+            on the board), and progress bars.
             """
         )
+
+        st.image("./resources/experience/vestaboard_improvements.png")
+
+
 
 # for testing purposes
 thing = RenderVestaboardPage()
