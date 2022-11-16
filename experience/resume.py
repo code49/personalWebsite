213a39e45a4e44e2b90737a0c0f0eccf37cd0d6c
code49 <david.le.chan@gmail.com>
2022-11-16 16:00:31 -0500

    updated resume page to include up-to-date versions

--- experience/resume.py
+++ experience/resume.py
@@ -63,15 +63,33 @@ class RenderResumePage(HydraHeadApp):
         pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="1000" height="800" type="application/pdf"></iframe>'
         st.markdown(pdf_display, unsafe_allow_html=True)
 
-        # --- section 3: download button ---
+        # --- section 3: download buttons ---
 
-        with open("./resources/experience/davidchan-resume.pdf", "rb") as pdf_file:
-            PDFbyte = pdf_file.read()
+        with open("./resources/experience/resume_digital.pdf", "rb") as pdf_file:
+            PDF_byte_digital = pdf_file.read()
 
         st.download_button(
-            label="Download PDF",
-            data=PDFbyte,
-            file_name='davidchan-resume.pdf'
+            label="Download Digital PDF",
+            data=PDF_byte_digital,
+            file_name='davidchan-resume-digital.pdf'
+        )
+
+        with open("./resources/experience/resume_printable.pdf", "rb") as pdf_file:
+            PDF_byte_printable = pdf_file.read()
+
+        st.download_button(
+            label="Download Printable PDF",
+            data=PDF_byte_printable,
+            file_name='davidchan-resume-printable.pdf'
+        )
+
+        with open("./resources/experience/resume_simple.pdf", "rb") as pdf_file:
+            PDF_byte_simple = pdf_file.read()
+
+        st.download_button(
+            label="Download Simple PDF",
+            data=PDF_byte_simple,
+            file_name='davidchan-resume-simple.pdf'
         )
 
 # for testing purpsoes
